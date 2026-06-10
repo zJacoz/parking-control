7ec389bf8209f898592e3a5674fadc2a9e9bf22f
zJacoz <jaco.net91@gmail.com>
2026-06-10 20:08:55 -0300

    Project created

---
+++ src/main/java/com/api/parking_control/ParkingControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.parking_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlApplication.class, args);
+	}
+
+}
