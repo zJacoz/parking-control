c96b6f74d9a4fdd39a70b736bcd8edb98c967681
zJacoz <jaco.net91@gmail.com>
2026-06-11 10:10:44 -0300

    Post and get methods created

--- src/main/java/com/api/parking_control/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parking_control/controllers/ParkingSpotController.java
@@ -2,11 +2,16 @@ package com.api.parking_control.controllers;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,10 +36,36 @@ public class ParkingSpotController {
 	
 	@PostMapping
 	public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto) {
+		
+		if(parkingSpotService.existsByLicensePlateCar(parkingSpotDto.getLicensePlateCar())) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: License Plate Car is already in use.");
+		}
+		if(parkingSpotService.existsByParkingSpotNumber(parkingSpotDto.getParkingSpotNumber())) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot Number is not able.");
+		}
+		if(parkingSpotService.existsByApartmentAndBlock(parkingSpotDto.getApartment(), parkingSpotDto.getBlock())) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot already registered with this Apartment and Block number.");
+		}
+		
 		var parkingSpotModel = new ParkingSpotModel();
 		BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
 		parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
 		return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
 		
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ParkingSpotModel>> getAllParkingSpots() {
+		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> getOneParkingSpot(UUID id) {
+		Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+		if(!parkingSpotModelOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
 	}
 }
--- src/main/java/com/api/parking_control/repositories/ParkingSpotRepository.java
+++ src/main/java/com/api/parking_control/repositories/ParkingSpotRepository.java
@@ -3,11 +3,14 @@ package com.api.parking_control.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Repository;
 
 import com.api.parking_control.models.ParkingSpotModel;
 
 @Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
-	
+	public boolean existsByLicensePlateCar(String licensePlateCar);
+	public boolean existsByParkingSpotNumber(String parkingSpotNumber);
+	public boolean existsByApartmentAndBlock(String apartment, String block);
 }
